bb0066c3a5e6e410f21d7b4b564edf56c8128674
mikojuzandra <mikojuzandra@gmail.com>
2021-09-04 19:10:37 +0700

    commit

---
+++ application/controllers/List_pokemon.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class List_pokemon extends CI_Controller {
+
+	public function index()
+	{
+        $url="https://pokeapi.co/api/v2/pokemon";
+        $get_url = file_get_contents($url);
+        $data = json_decode($get_url);
+
+        $data_array = array(
+            'datalist' => $data
+        );
+        $this->load->view('pokemon/list_pokemon',$data_array);
+	}
+
+    public function data($url)
+    {
+        $url="https://pokeapi.co/api/v2/pokemon/".$url;
+        $get_url = file_get_contents($url);
+        $data = json_decode($get_url);
+
+        $data_array = array(
+            'datalist' => $data
+        );
+        $this->load->view('pokemon/list_pokemon_detail',$data_array);
+    }
+}
---
+++ application/views/pokemon/list_pokemon.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Pokemon</title>
+
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="bg-primary py-5">
+        <div class="container">
+            <h1 class="display-4 text-white">Hello Pokemon!</h1>
+        </div>
+    </header>
+        <div class="row">
+            <div class="col-sm-12">
+        <?php  
+
+            $start = 0;
+            echo "<table id='tabel-data' class='table table-striped table-bordered' width='100%' cellspacing='0'><tr><td>Nomor</td><td>Nama Pokemon</td><td>Detail</td>";
+            foreach ($datalist->results as $value)
+            {
+            ?>
+                <tr>
+                <td><?php echo ++$start ?></td>
+                <td><?php echo $value->name;?></td>
+                <td><a href="<?php echo site_url()."List_pokemon/data/".$start; ?>">Lihat</a></td>
+                </tr>
+            <?php
+            }
+            echo "</tr></table>";
+        ?>
+        </div>
+        </div>
+ 
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+        crossorigin="anonymous"></script>
+        <script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript">
+             $(document).ready(function(){
+                $('#tabel-data').DataTable();
+            });
+        </script>
+</body>
+
+</html>
+
